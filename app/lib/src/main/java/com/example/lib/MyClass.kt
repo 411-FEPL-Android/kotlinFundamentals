15990055329de7d49799190247d21e305d326578
Fernando Pérez <fpl81660@lasallebajio.edu.mx>
2026-02-11 17:37:42 -0600

    first commit

---
+++ app/lib/src/main/java/com/example/lib/MyClass.kt
@@ -0,0 +1,47 @@
+package com.example.lib
+//1.Crea un programa que tenga un precio de comida
+// y un porcentaje de propina e imprimir cuanto es el total a pagar
+fun main() {
+    println("hola mundo")
+    //1.
+//    val price : Double =150.0
+//    val porcentaje : Double =0.10
+//    val tip : Double = price * porcentaje
+//    val total : Double = price + tip
+//    println("el total a pagar es $total")
+//El portero de la discoteca verifica que una persona pueda entrar en la discoteca
+ //   solo si es mayor de edad
+//
+//    //rangos
+//    for (i in 1..10){
+//        println(i)
+//    }
+//    for (i in 10 downTo 2){
+//        println(i)
+//    }
+//    for (i in 100 downTo 0 step 10){
+//        println(i)
+//    }
+//
+//    println("ingrese su edad")
+//    val imput = readlnOrNull()
+//    val age = imput?.toIntOrNull()
+//
+//    when(age){
+//        in 18.. Integer.MAX_VALUE -> println("puede entrar")
+//        in 0..17 -> println("no puede entrar")
+//        null -> println("ingrese un numero valido")
+//    }
+var counter : Int = 12
+while (counter < 20){
+    println("El contador es menor que 20")
+    counter++
+
+}
+    do{
+        println("el contador es menor que 30")
+        counter++
+    }while (counter < 3)
+
+
+}
